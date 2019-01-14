c568ca1e17530b3c2aee44c33c88d6e33759743a
Chi-AnTai <andy.chian.tai@gmail.com>
2019-01-14 18:16:17 +0800

    add statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp()); 
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List _products = ['Nightwish'];
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/nightwish.jpg'),
-          Text('NightWish')
-        ],),),
-      ),
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add product'),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map(
+                      (element) => Card(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('assets/nightwish.jpg'),
+                                Text(element)
+                              ],
+                            ),
+                          ),
+                    )
+                    .toList(),
+              )
+            ],
+          )),
     );
   }
 }
